1a778e2a3856f71d1f4afe3279319c19023e7c5c
farhaan104 <farhaan104@gmail.com>
2015-11-29 13:05:12 -0500

    adding password saving capability

--- ChatServer.java
+++ ChatServer.java
@@ -13,10 +13,11 @@ public class ChatServer {
 		int portNumber = 9000;;
 		
 		if (args.length < 1) {
-			System.out.println("Usage: java ChatServer <port number=9000>");
+			System.out.println("Usage: $ java ChatServer <port number>");
+			System.out.println("e.g.: $ java ChatServer 9000");
 			return;
 		}
-		else if (args.length == 1)
+		else
 			portNumber = Integer.parseInt(args[0]);
 
 		try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
@@ -35,8 +36,8 @@ public class ChatServer {
 	private static class Handler extends Thread {
 		private Socket clientSocket;
 		private PrintWriter out;
-		private BufferedReader in;
 		String name;
+		HashMap <String, String> credentials = new HashMap<String, String>();
 		
 		Handler(Socket s) {
 			clientSocket = s;
@@ -44,8 +45,9 @@ public class ChatServer {
 		
 		public void run() {
 			try {
-			
-				in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				String credentialsFile = "UserCredentials.csv";
+				readCredentials(credentialsFile);
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 				out = new PrintWriter(clientSocket.getOutputStream(), true);
 			
 				while (true) {
@@ -86,6 +88,21 @@ public class ChatServer {
 				} catch (IOException e) {
 				}
 			}
+		}
+
+		private HashMap <String, String> readCredentials(String credentialsFile) {
+			File file = new File(credentialsFile);
+			HashMap <String, String> credentials = new HashMap<String, String>();
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				br.readLine();
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return credentials;
 		}	
 	}
 
